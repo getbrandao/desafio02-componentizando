7841c287d08567dae78af558902a45df2a85ee57
Gustavo Brandão <gustavobrandao@bsd.com.br>
2021-11-30 09:54:20 -0300

    Desafio02 - Completo

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,30 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+import '../styles/sidebar.scss';
+
+interface SiderProps {
+  genres: {
+    id: number,
+    title: string,
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family',
+  }[],
+  handleClickButton(id: number): void,
+  selectedGenreId: number,
+}
+
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: SiderProps) {
+  return (<nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+    <div className="buttons-container">
+      {genres.map(genre => (
+        <Button
+          key={String(genre.id)}
+          title={genre.title}
+          iconName={genre.name}
+          onClick={() => handleClickButton(genre.id)}
+          selected={selectedGenreId === genre.id}
+        />
+      ))}
+    </div>
+  </nav>)
 }
